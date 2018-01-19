d6e1e0e06c150a765bde2c22486c88bb7cd47a09
jay <jamesjayko@gmail.com>
2018-01-19 12:14:32 -0800

    init commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import 'materialize-css/dist/css/materialize.min.css'
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+     <h1 className="center-align">user auth</h1>
     </div>
 );
 
