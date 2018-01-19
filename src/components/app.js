a6acd5e9be7ea79f7b1c84e477c58d5b1d16beb0
jay <jamesjayko@gmail.com>
2018-01-19 14:54:39 -0800

    finished navbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import 'materialize-css/dist/css/materialize.min.css'
+import React from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import Nav from "./nav";
 
 const App = () => (
-    <div>
-     <h1 className="center-align">user auth</h1>
-    </div>
+  <div>
+    <Nav />
+    <h1 className="center-align">user auth</h1>
+  </div>
 );
 
 export default App;
