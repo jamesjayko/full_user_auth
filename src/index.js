b3d679ad40bd7d85a6afc9f8b68aaca1f637aa3c
jay <jamesjayko@gmail.com>
2018-01-19 12:39:20 -0800

    added redux setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducers from "./reducers";
 
-import App from './components/app';
+import App from "./components/app";
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+  <Provider store={createStore(rootReducers, {}, applyMiddleware(thunk))}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
